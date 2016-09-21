0da2e5cfafa3f1e26e783553807e369f28e07366
Gojko Adzic <gojko@gojko.com>
2016-09-21 04:05:32 +0200

    traversal with levels

--- spec/traverse-spec.js
+++ spec/traverse-spec.js
@@ -5,37 +5,49 @@ describe('traverse', function () {
 	describe('when version is not specified (so non root nodes or v2)', function () {
 		it('applies a depth-first, pre-order traversal', function () {
 			var content = {  id: 1, ideas: { '11': {id: 11, ideas: { 1: { id: 111}, 2: {id: 112} } }, '-12': {id: 12, ideas: { 1: {id: 121} } }, '-13' : {id: 13} } },
-			result = [];
-			traverse(content, function (idea) {
+				result = [],
+				levels = [];
+			traverse(content, function (idea, level) {
 				result.push(idea.id);
+				levels.push(level);
 			});
 			expect(result).toEqual([1, 11, 111, 112, 12, 121, 13]);
+			expect(levels).toEqual([1, 2, 3, 3, 2, 3, 2]);
 		});
 		it('does a post-order traversal if second argument is true', function () {
 			var content = { id: 1, ideas: { '11': {id: 11, ideas: { 1: { id: 111}, 2: {id: 112} } }, '-12': {id: 12, ideas: { 1: {id: 121} } }, '-13' : {id: 13} } },
-			result = [];
-			traverse(content, function (idea) {
+				result = [],
+				levels = [];
+			traverse(content, function (idea, level) {
 				result.push(idea.id);
+				levels.push(level);
 			}, true);
 			expect(result).toEqual([111, 112, 11, 121, 12, 13, 1]);
+			expect(levels).toEqual([3, 3, 2, 3, 2, 2, 1]);
 		});
 	});
 	describe('v3', function () {
 		it('skips root node in preorder traversal', function () {
 			var content = { formatVersion: 3, id: 1, ideas: { '11': {id: 11, ideas: { 1: { id: 111}, 2: {id: 112} } }, '-12': {id: 12, ideas: { 1: {id: 121} } }, '-13' : {id: 13} } },
-			result = [];
-			traverse(content, function (idea) {
+				result = [],
+				levels = [];
+			traverse(content, function (idea, level) {
 				result.push(idea.id);
+				levels.push(level);
 			});
 			expect(result).toEqual([11, 111, 112, 12, 121, 13]);
+			expect(levels).toEqual([1, 2, 2, 1, 2, 1]);
 		});
 		it('skips root node in postoder traversal', function () {
 			var content = { id: 1, formatVersion: 3, ideas: { '11': {id: 11, ideas: { 1: { id: 111}, 2: {id: 112} } }, '-12': {id: 12, ideas: { 1: {id: 121} } }, '-13' : {id: 13} } },
-			result = [];
-			traverse(content, function (idea) {
+				result = [],
+				levels = [];
+			traverse(content, function (idea, level) {
 				result.push(idea.id);
+				levels.push(level);
 			}, true);
 			expect(result).toEqual([111, 112, 11, 121, 12, 13]);
+			expect(levels).toEqual([2, 2, 1, 2, 1, 1]);
 		});
 	});
 });
--- src/traverse.js
+++ src/traverse.js
@@ -1,15 +1,17 @@
 /*global module, require */
 var sortedSubIdeas = require('./sorted-sub-ideas');
-module.exports = function traverse(contentIdea, iterator, postOrder) {
+module.exports = function traverse(contentIdea, iterator, postOrder, level) {
 	'use strict';
-	if (!postOrder && (!contentIdea.formatVersion || contentIdea.formatVersion < 3)) {
-		iterator(contentIdea);
+	var isSingleRootMap = !level && (!contentIdea.formatVersion || contentIdea.formatVersion < 3);
+	level = level || (isSingleRootMap ? 1 : 0);
+	if (!postOrder && (isSingleRootMap || level)) {
+		iterator(contentIdea, level);
 	}
 	sortedSubIdeas(contentIdea).forEach(function (subIdea) {
-		traverse(subIdea, iterator, postOrder);
+		traverse(subIdea, iterator, postOrder, level + 1);
 	});
-	if (postOrder && (!contentIdea.formatVersion || contentIdea.formatVersion < 3)) {
-		iterator(contentIdea);
+	if (postOrder && (isSingleRootMap || level)) {
+		iterator(contentIdea, level);
 	}
 };
 
